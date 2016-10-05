28b267d5795dfa12e5ba6ca6091c64ed66fb12d8
SeanHouser <seanmhouser@gmail.com>
2016-10-05 13:20:06 -0500

    Started Challenge 286
    no work done

---
+++ Challenge286/Challenge286/Challenge286.cpp
@@ -0,0 +1,12 @@
+// Challenge286.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Please enter the number you'd like to find the factorial of" << std::endl;
+	return 0;
+}
+
