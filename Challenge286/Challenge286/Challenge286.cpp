01c042b4064e667be7fffeed2314638b48581685
SeanHouser <seanmhouser@gmail.com>
2016-10-05 14:22:28 -0500

    Challenge 286 SOLVED
    used lazy recursion (cout at bottom of stack rather than returning, need
    more practice)

--- Challenge286/Challenge286/Challenge286.cpp
+++ Challenge286/Challenge286/Challenge286.cpp
@@ -1,12 +1,35 @@
-// Challenge286.cpp : Defines the entry point for the console application.
+// Challenge286.cpp : Defines the entry pofloat for the console application.
 //
 
 #include "stdafx.h"
 #include <iostream>
 
-int _tmain(int argc, _TCHAR* argv[])
+void rev_factorial(float, float);
+
+float _tmain(float argc, _TCHAR* argv[])
 {
-	std::cout << "Please enter the number you'd like to find the factorial of" << std::endl;
+	float number;
+	std::cout << "Press Ctrl+C to exit\nPlease enter the number you'd like to find the factorial of" << std::endl;
+	while (1) {
+	std::cin >> number;
+	rev_factorial(number, 2);
+	}
 	return 0;
 }
 
+void rev_factorial(float number, float factorial) {
+	if (number < 1) {
+		std::cout << "NONE" << std::endl;
+		return;
+	}
+	if (number == 1) {
+		factorial--;
+		std::cout << "The factorial is: " << factorial << std::endl;
+		return;
+	}
+	else {
+		number = number/factorial;
+		factorial++;
+		rev_factorial(number, factorial);
+	}
+}
