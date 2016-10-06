6748fa7ff14b31dfd5273040dae3ddce9560ea21
SeanHouser <seanmhouser@gmail.com>
2016-10-06 10:36:04 -0500

    Functional
    Using python, recursive solution semantics are functional. May want to
    practice cleaning up.

--- Challenge286 Python/Challenge286 Python/Challenge286_Python.py
+++ Challenge286 Python/Challenge286 Python/Challenge286_Python.py
@@ -1,2 +1,17 @@
 number = input("Please enter the number you would like to find the reverse factorial of\n")
-
+factorial = 2
+def rev_factorial(number, factorial):
+    if number == 1:
+        factorial = factorial - 1
+        return factorial
+    elif number < 1:
+        return -99
+    else:
+        number = number/factorial
+        factorial = factorial + 1 
+        return rev_factorial(number, factorial)
+answer = rev_factorial(number, 2)
+if answer == -99:
+    print "NONE"
+else:
+    print "Factorial is ", answer, "!\n"
