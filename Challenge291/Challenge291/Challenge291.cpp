dfbe9526049a1f777a05d9083db324eabb1b210d
SeanHouser <seanmhouser@gmail.com>
2016-11-14 16:38:59 -0600

    Update
    read in and parse to vector done

--- Challenge291/Challenge291/Challenge291.cpp
+++ Challenge291/Challenge291/Challenge291.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
 
+int popFront(std::vector<int>);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::ifstream input;
+	int s;
+	int g_weight;
+	int g_temp;
+	input.open("input.txt");
+	std::vector<int> food_info;
+	for (int i=0; input; ++i) {
+		input >> s;
+		food_info.push_back(s);
+	}
+	popFront(food_info);
+
+	input.close();
 	return 0;
+}
+
+int popFront(std::vector<int> & food_info)
+{
+	int temp;
+	temp = *food_info.begin();
+	food_info.erase(food_info.begin());
+	return temp;
 }
 
