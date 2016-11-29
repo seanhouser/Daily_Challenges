e1e3b97a9f1576edce8f57c1b5690f52d59f438e
SeanHouser <seanmhouser@gmail.com>
2016-11-29 17:35:55 -0600

    Challenge 293 update
    file read complete

--- Challenge293/Challenge293/Challenge293.cpp
+++ Challenge293/Challenge293/Challenge293.cpp
@@ -2,9 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+bool defuse_step(std::string color) {
+
+	return true;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::string s;
+
+	std::ifstream input("input.txt");
+	while (getline(input, s)) {
+		if (s.empty()) {
+			std::cout << "Line is empty\n";
+			//start state machine over
+		}
+		else {
+			std::cout << s << '\n';
+		}
+	}
+
+
 	return 0;
 }
 
