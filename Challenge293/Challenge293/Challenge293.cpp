5520adee4e6c3b8ad7250a23b2170cfb58c8aacc
SeanHouser <seanmhouser@gmail.com>
2016-12-01 16:47:27 -0600

    Challenge 293 progress
    Configured Enum, began switch process

--- Challenge293/Challenge293/Challenge293.cpp
+++ Challenge293/Challenge293/Challenge293.cpp
@@ -5,27 +5,54 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <vector>
 
-bool defuse_step(std::string color) {
+enum Color{white, black, purple, red, green, orange, SUCCESS};
+std::vector <std::string> defusal;
 
-	return true;
+Color set_step(std::string color) {
+	if (color == "white")
+		return white;
+	else if (color == "black")
+		return black;
+	else if (color == "purple")
+		return purple;
+	else if (color == "red")
+		return red;
+	else if (color == "orange")
+		return orange;
+	else
+		return SUCCESS;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::string s;
-
 	std::ifstream input("input.txt");
-	while (getline(input, s)) {
-		if (s.empty()) {
-			std::cout << "Line is empty\n";
-			//start state machine over
-		}
-		else {
-			std::cout << s << '\n';
+	
+	while (getline(input, s))
+		defusal.push_back(s);
+
+	for (int i=0; i<defusal.size(); ++i) {
+		Color step, nextStep;
+		step = set_step(defusal[i]);
+		nextStep = set_step(defusal[i+1]);
+
+		switch (step){
+		case white:
+			if (nextStep == white || nextStep == black) {
+				std::cout << "BOOM\n";
+				return 0;
+			}
+		case black:
+		case purple:
+		case red:
+		case green:
+		case orange:
+		case SUCCESS:
+			std::cout << "Successfully defused\n";
 		}
 	}
-
 
 	return 0;
 }
