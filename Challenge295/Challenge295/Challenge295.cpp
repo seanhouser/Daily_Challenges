9892420955c09ad71b29a5cb3fc66e84ae5c7b9b
SeanHouser <seanmhouser@gmail.com>
2016-12-12 15:58:21 -0600

    New challenge initiated

---
+++ Challenge295/Challenge295/Challenge295.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::ifstream input("input.txt");
+	char c;
+	while (input) {
+		
+	}
+
+	return 0;
+}
+
