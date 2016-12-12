be74648cbee9c653483f5560f411a70c9c8eb804
SeanHouser <seanmhouser@gmail.com>
2016-12-12 17:23:42 -0600

    Challenge 295 SOLVED Final

---
+++ Challenge295 - SOLVED/Challenge295/Challenge295.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::ifstream input("input.txt");
+	std::vector<char> data;
+	char s;
+
+	while (input >> std::noskipws >> s)
+		data.push_back(s);
+
+	for (int i=0; i<data.size()/2+1; ++i) {
+		for (int j=0; j<(data.size()/2); ++j) {
+				if (j+1 <= i)
+					std::cout << data[j+(data.size()/2+1)];
+				else 
+					std::cout << data[j];
+		}
+		std::cout << '\n';
+	}
+	return 0;
+}
+
