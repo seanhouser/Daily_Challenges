c1bcd684b2411033a85c2f7f61ec390b008dd880
SeanHouser <seanmhouser@gmail.com>
2017-02-08 15:20:10 -0600

    Progress
    vector loaded with structs of elements

--- Challenge302/Challenge302/Challenge302.cpp
+++ Challenge302/Challenge302/Challenge302.cpp
@@ -2,18 +2,41 @@
 //
 
 #include "stdafx.h"
+#include <fstream>
 #include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
 
-struct element {
-	std::string name;
-	std::string symbol;
-	int a_weight;
+struct element {			//structure to store each element
+	std::string name;		//name of checmical element
+	std::string symbol;		//symbol of checmical element
+	std::string a_weight;	//atomic weight of checmical element
 };
 
-element p_table[100];
+std::vector<element> p_table;	//vector to store all elements structures
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::ifstream input("p_table.txt");	//steam for periodic table input
+	std::string temp;					//string for storing each line of periodic table input
+
+	for (int i=0; getline(input, temp); ++i) {
+		std::vector<std::string> buffer_vector;		//vector for storing individual words of each line
+		std::string buffer;							//string buffer
+		std::stringstream ss(temp);					//insert buffer string into stream
+		while (ss >> buffer) {
+			buffer_vector.push_back(buffer);		//load vector with separated words
+		}
+		element e;						//create new element structure and load data
+		e.name = buffer_vector[0];
+		e.symbol = buffer_vector[1];
+		e.a_weight = buffer_vector[3];
+		p_table.push_back(e);			//push loaded structure into p_table vector
+	}
+
+
+
 	return 0;
 }
 
