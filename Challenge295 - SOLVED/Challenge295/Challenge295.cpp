a76039c9bf2fa7e067d7eaffefc866c8c32cb81f
SeanHouser <seanmhouser@gmail.com>
2017-02-08 14:17:54 -0600

    Started challenge 302

--- Challenge295 - SOLVED/Challenge295/Challenge295.cpp
+++ Challenge295 - SOLVED/Challenge295/Challenge295.cpp
@@ -22,11 +22,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	while (input >> std::noskipws >> s)
 		data.push_back(s);
 
-	for (int i=0; i<data.size()/2+1; ++i) {
-		if (i == data.size()/2)
+	for (int i=0; i<data.size()/2+1; ++i)
+		if (i == data.size()/2 || data[i] != data[i+data.size()/2+1])
 			printWord(i, data);
-		else if (data[i] != data[i+data.size()/2+1])
-			printWord(i, data);
-	}
+
 	return 0;
 }
---
+++ Challenge302/Challenge302/Challenge302.cpp
@@ -0,0 +1,19 @@
+// Challenge302.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+
+struct element {
+	std::string name;
+	std::string symbol;
+	int a_weight;
+};
+
+element p_table[100];
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
