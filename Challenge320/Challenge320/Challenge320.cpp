38d0716ebd5ca0211df4a9dbb6b4bca7319f8494
SeanHouser <seanmhouser@gmail.com>
2017-06-26 12:43:33 -0600

    Challenge 320 Progress

--- Challenge320/Challenge320/Challenge320.cpp
+++ Challenge320/Challenge320/Challenge320.cpp
@@ -5,19 +5,28 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int spiral_num;
 	int spiral_size;
+	std::vector<int> numbers;
 
 	std::cout << "Please enter the number you would like spiraled out\n";
 	std:: cin >> spiral_num;
 	spiral_size = spiral_num*spiral_num;
 
-	for (int i=1; i<spiral_size; i++) {
-		std:: cout << i << ' ';
-		if (i%spiral_num == 0)
+	for (int i=1; i<spiral_size+1; i++) {
+			numbers.push_back(i);
+		
+	}
+
+
+	for (int i=0; i<spiral_size; i++) {
+		std:: cout << numbers[i] << ' ';
+		if (i%spiral_num == 0 && i != 0)
 			std::cout << '\n';
 	}
 
