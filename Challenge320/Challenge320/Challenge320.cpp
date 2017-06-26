f74732a732f9543a99e3af263b61ed6ecb35ed02
SeanHouser <seanmhouser@gmail.com>
2017-06-26 11:14:22 -0600

    Challenge 320 progress
    Easy boilerplate completed

--- Challenge320/Challenge320/Challenge320.cpp
+++ Challenge320/Challenge320/Challenge320.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
-
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int spiral_num;
+	int spiral_size;
+
+	std::cout << "Please enter the number you would like spiraled out\n";
+	std:: cin >> spiral_num;
+	spiral_size = spiral_num*spiral_num;
+
+	for (int i=1; i<spiral_size; i++) {
+		std:: cout << i << ' ';
+		if (i%spiral_num == 0)
+			std::cout << '\n';
+	}
+
 	return 0;
 }
 
