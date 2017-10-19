0712958b8c163c6cc965e6d83a32a69198135536
SeanHouser <seanmhouser@gmail.com>
2017-10-19 12:08:46 -0600

    Began new challenge

---
+++ Challenge336/Challenge336/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge336
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of values given followed by the number of queries");
+            int i = Console.ReadLine();
+            Console.WriteLine("{1}", i);
+        }
+    }
+}
